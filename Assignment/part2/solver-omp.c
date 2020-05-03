c0814b865d204967778240c61b118d660c025346
axelwass <axelwass@gmail.com>
2020-05-03 18:11:10 +0200

    Parallelization for relaxgauss method

--- Assignment/part2/solver-omp.c
+++ Assignment/part2/solver-omp.c
@@ -93,20 +93,28 @@ double relax_gauss (double *u, unsigned sizex, unsigned sizey)
     bx = sizex/nbx;
     nby = NB;
     by = sizey/nby;
-    for (int ii=0; ii<nbx; ii++) //PARALLELIZE
+	
+	int block[nbx][nby];
+#pragma omp parallel
+#pragma omp single
+    for (int ii=0; ii<nbx; ii++)
         for (int jj=0; jj<nby; jj++)
-
           // SYNC to check if previous block is computed
-            for (int i=1+ii*bx; i<=min((ii+1)*bx, sizex-2); i++)
-                for (int j=1+jj*by; j<=min((jj+1)*by, sizey-2); j++) {
-	            unew= 0.25 * (    u[ i*sizey	+ (j-1) ]+  // left
-				      u[ i*sizey	+ (j+1) ]+  // right
-				      u[ (i-1)*sizey	+ j     ]+  // top
-				      u[ (i+1)*sizey	+ j     ]); // bottom
-	            diff = unew - u[i*sizey+ j];
-	            sum += diff * diff;
-	            u[i*sizey+j]=unew;
-                }
-            // SYNC to signal that this block is computed
+#pragma omp task depend(in : block[ii-1][jj], block[ii][jj-1]) depend(out : block[ii][jj]) private(diff ,unew)
+			{
+				double temp_sum = 0.0;
+				for (int i=1+ii*bx; i<=min((ii+1)*bx, sizex-2); i++)
+					for (int j=1+jj*by; j<=min((jj+1)*by, sizey-2); j++) {
+					unew= 0.25 * (    u[ i*sizey	+ (j-1) ]+  // left
+						  u[ i*sizey	+ (j+1) ]+  // right
+						  u[ (i-1)*sizey	+ j     ]+  // top
+						  u[ (i+1)*sizey	+ j     ]); // bottom
+					diff = unew - u[i*sizey+ j];
+					temp_sum += diff * diff;
+					u[i*sizey+j]=unew;
+					}
+				#pragma omp atomic update
+				sum += temp_sum;
+			}
     return sum;
 }
